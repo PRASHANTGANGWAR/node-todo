875b3328714eb81d2fe11264175de99dcf4be482
Atul Singh <atul.singh@sofocle.com>
2018-09-07 20:22:45 +0530

    creating API

---
+++ app.js
@@ -0,0 +1,99 @@
+//packages  needed
+var bodyParser = require('body-parser'); // for taking input
+var express = require('express');
+var app = express();
+var router = express.Router();
+app.set('view engine','ejs');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use( express.static('./'));
+
+var router = express.Router(); // for routing 
+
+ app.get('/loadall',(req,res)=>{
+    //  return res.send('hello world')
+    dbo.collection("tasks").find({}).toArray(function(err, result) {
+        if (err) throw err;
+       return res.send(result);
+        db.close();
+      });
+ })
+
+//pp code
+//  app.get('/loadall',async(req,res)=>{
+//     //  return res.send('hello world')
+//     dbo.collection("tasks").find({}).toArray((err, result) => {
+//         if (err) throw err;
+//        return res.send(result);
+//         db.close();
+//       });
+//  })
+var id =0;
+ app.get('/findone/:id',(req,res)=>{
+    var id = req.body;
+    var id = req.params.id;
+
+    dbo.collection("tasks").find({"_id":id}).toArray(function(err, result) {
+        if (err) throw err;
+       return res.send(result);
+        db.close();
+      });
+ })
+
+ app.post('/add-task', (req,res)=> {
+    res.send({type:'POST'});
+
+    var a = req.body;
+    console.log(a);
+ })
+
+
+ app.post('/tasks', urlencodedParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    myobj=req.body;
+        dbo.collection("tasks").insertOne(myobj, function(err, resp) {
+            if (err) throw err;
+            console.log("1 document inserted",resp);
+            res.send('welcome, ' + req.body.taskname)
+            //db.close();
+          });
+    
+  })
+
+var todocontroller = require('./controllers/todo-controller');
+todocontroller(app);
+
+//application running at 8081 
+app.listen(8080, ()=>{
+    console.log("listining on port 8080");
+})
+
+// rendering ejs page  or getting data from api
+app.get('/test', function(req, res) {
+    res.render('todo.ejs', {root:__dirname,abc:"test1" ,hello:"hello" })
+});
+
+//connecting to database
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/todo-db";
+var dbo
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+   dbo = db.db("todo-db"); //db todo-db created
+  console.log("Database created!"+dbo);
+
+  //collection is table created tasks
+    dbo.createCollection("tasks", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created! tasks");
+    });
+
+
+        //   dbo.collection("tasks").insertOne(myobj, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 document inserted",res);
+        //     db.close();
+        //   });
+
+});
---
+++ controllers/todo-controller.js
@@ -0,0 +1,11 @@
+module.exports = function(app)
+{
+    app.get('/test', function(req, res) {
+    });
+
+    app.set('/test', function(req, res) {
+    });
+
+    app.delete('/test', function(req, res) {
+    });
+}
